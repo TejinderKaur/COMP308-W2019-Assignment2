f785c2ca14f8c30aaddbbecf6fafb3a2d80cf9bc
Tejinder Kaur <kaurtejinder31095@gmail.com>
2019-02-15 16:47:57 -0500

    home page added

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+router.get('/project', function(req, res, next) {
+  res.render('index', { title: 'Project' });
+});
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+
+module.exports = router;
